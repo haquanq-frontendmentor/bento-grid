3cfcaa827b4c65006390871c918d8f5148998974
Ha Quanq <quangha0120010@gmail.com>
2024-09-23 13:32:06 +0700

    Re-format files tab width

--- webpack.config.js
+++ webpack.config.js
@@ -6,40 +6,40 @@ const WebpackCopyPlugin = require("copy-webpack-plugin");
 
 /** @type { import('webpack').Configuration } */
 module.exports = {
-    mode: "development",
-    entry: "./src/styles/main.css",
-    output: {
-        path: path.resolve(__dirname, "dist"),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(css)$/,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
-                exclude: path.resolve(__dirname, "node_modules"),
-            },
-        ],
-    },
-    devServer: {
-        static: {
-            directory: path.resolve(__dirname, "dist"),
-        },
-        port: 3000,
-    },
-    plugins: [
-        new WebpackRemoveEmptyScript(),
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            filename: "index.html",
-            template: "src/index.html",
-        }),
-        new WebpackCopyPlugin({
-            patterns: [
-                {
-                    from: "./src/assets/",
-                    to: "assets/",
-                },
-            ],
-        }),
+  mode: "development",
+  entry: "./src/styles/main.css",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(css)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader"],
+        exclude: path.resolve(__dirname, "node_modules"),
+      },
     ],
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, "dist"),
+    },
+    port: 3000,
+  },
+  plugins: [
+    new WebpackRemoveEmptyScript(),
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "src/index.html",
+    }),
+    new WebpackCopyPlugin({
+      patterns: [
+        {
+          from: "./src/assets/",
+          to: "assets/",
+        },
+      ],
+    }),
+  ],
 };
