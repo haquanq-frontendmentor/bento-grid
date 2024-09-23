b581501903e7db4327691d1ce072cb0780a61199
Ha Quanq <quangha0120010@gmail.com>
2024-09-23 17:08:36 +0700

    Add plugin for sorting/combining media queries

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
 module.exports = {
-    plugins: [require("postcss-import"), require("postcss-nested"), require("cssnano"), require("autoprefixer")],
+  plugins: [
+    require("postcss-import"),
+    require("postcss-nested"),
+    require("cssnano"),
+    require("autoprefixer"),
+    require("postcss-sort-media-queries")({
+      sort: "mobile-first",
+    }),
+  ],
 };
