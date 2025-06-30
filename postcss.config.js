e6765371c51d6689908a1389727a440401bea2e7
Ha Quanq <quangha0120010@gmail.com>
2025-06-30 16:15:53 +0700

    Migrate from Webpack to Parcel

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,12 @@
 /** @type {import('postcss-load-config').Config} */
-module.exports = {
-  plugins: [
-    require("postcss-import"),
-    require("postcss-nested"),
-    require("cssnano"),
-    require("autoprefixer"),
-    require("postcss-sort-media-queries")({
-      sort: "mobile-first",
-    }),
-  ],
+export default {
+    plugins: [
+        require("postcss-import"),
+        require("postcss-nested"),
+        require("cssnano"),
+        require("autoprefixer"),
+        require("postcss-sort-media-queries")({
+            sort: "mobile-first",
+        }),
+    ],
 };
